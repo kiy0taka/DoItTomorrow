1d31157781f8b5ed220c6b758ae1be484a561482
kiy0taka <kiy0taka333+github@gmail.com>
2009-11-17 00:21:49 +0900

    add comments

--- war/WEB-INF/groovy/list.groovy
+++ war/WEB-INF/groovy/list.groovy
@@ -4,22 +4,29 @@ import static com.google.appengine.api.datastore.Query.FilterOperator.*
 import static org.apache.commons.lang.time.DateUtils.*
 
 def query = new Query('tweet')
+
+// order by timestamp
 query.addSort("createdAt", Query.SortDirection.DESCENDING)
+
+// filter with user
 if (params.user) query.addFilter("fromUser", EQUAL, params.user)
+
+// filter with status (today|tomorrow)
 if (params.status == 'today') {
-    def from = addHours(truncate(addDays(new Date(), -1), Calendar.DATE), -9)
-    def to = addDays(from, 1)
+    def to = addHours(truncate(addHours(new Date(), 9), Calendar.DATE), -9)
+    def from = addDays(to, -1)
     query.addFilter("createdAt", GREATER_THAN_OR_EQUAL, from)
     query.addFilter("createdAt", Query.FilterOperator.LESS_THAN, to)
 } else if (params.status == 'tomorrow') {
-    def from = addHours(truncate(new Date(), Calendar.DATE), -9)
+    def from = addHours(truncate(addHours(new Date(), 9), Calendar.DATE), -9)
     def to = addDays(from, 1)
     query.addFilter("createdAt", GREATER_THAN_OR_EQUAL, from)
     query.addFilter("createdAt", Query.FilterOperator.LESS_THAN, to)
 }
-def pageNo = (params.page ?: '1').toInteger()
-def tweets = datastoreService.prepare(query).asList(withOffset((pageNo - 1) * 20).limit(21))
-request.hasNext = tweets.size() == 21
+
+def paging = withOffset(((params.page ?: '1').toInteger() - 1) * 20).limit(21)
+def tweets = datastoreService.prepare(query).asList(paging)
+request.hasNext = (tweets.size() == 21)
 request.tweets = request.hasNext ? tweets[0..-2] : tweets
 
 forward 'list.gtpl'
